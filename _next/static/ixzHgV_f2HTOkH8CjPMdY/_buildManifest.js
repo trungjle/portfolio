62ff14b4ee5ccc16b53a2d9787edfe835305d903
trungjle <62075882+trungjle@users.noreply.github.com>
2022-12-25 20:03:31 +0000

    Deploying to gh-pages from @ trungjle/portfolio-project@40446564b4700731254019987bb9835338fdf792 🚀

--- _next/static/ixzHgV_f2HTOkH8CjPMdY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5ce9822ff70f85fc.css","static/chunks/pages/index-bf7a3846e939fda0.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
