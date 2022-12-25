40446564b4700731254019987bb9835338fdf792
trungjle <tle1911@gmail.com>
2022-12-25 20:02:50 +0000

    Renamed basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     loader: "akamai",
     path: ""
   },
-  basePath: "/nextjs-pages",
-  assetPrefix: "/nextjs-pages",
+  basePath: "/portfolio-project",
+  assetPrefix: "/portfolio-project",
 }
 
 module.exports = nextConfig
