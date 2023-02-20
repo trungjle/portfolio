6553910f7e3e8e4c216294115460516108aa5c4f
trungjle <tle1911@gmail.com>
2023-02-20 11:32:06 +0000

    Updated node.js.yml

--- next.config.js
+++ next.config.js
@@ -8,8 +8,6 @@ const nextConfig = {
     loader: "akamai",
     path: "/",
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   trailingSlash: true,
 }
 
