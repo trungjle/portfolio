7322e230702dbe53927a74dc4b7656d35741c56e
trungjle <tle1911@gmail.com>
2023-02-21 20:24:22 +0000

    Removed node.js.yml

--- components/skills.tsx
+++ components/skills.tsx
@@ -12,35 +12,35 @@ const Skills = () => {
 
                 <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-8 text-center py-8 text-black">
                     <div className="shadow-md shadow-[#040c16] rounded-md hover:scale-110 duration-500 bg-[#ccd6f6]">
-                        <Image className="mx-auto my-4" width={80} height={80} src={"/HTML.png"} alt="HTML icon" unoptimized={true} />
+                        <Image className="mx-auto my-4" width={80} height={80} src={"/HTML.png"} alt="HTML icon" />
                         <p className="my-4">HTML</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] rounded-md hover:scale-110 duration-500 bg-[#ccd6f6]">
-                        <Image className="mx-auto my-4" width={80} height={80} src={"/CSS.png"} alt="CSS icon" unoptimized={true} />
+                        <Image className="mx-auto my-4" width={80} height={80} src={"/CSS.png"} alt="CSS icon" />
                         <p className="my-4">CSS</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] rounded-md hover:scale-110 duration-500 bg-[#ccd6f6]">
-                        <Image className="mx-auto my-4" width={80} height={80} src={"/React.png"} alt="React icon" unoptimized={true} />
+                        <Image className="mx-auto my-4" width={80} height={80} src={"/React.png"} alt="React icon" />
                         <p className="my-4">React</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] rounded-md hover:scale-110 duration-500 bg-[#ccd6f6]">
-                        <Image className="mx-auto my-4" width={80} height={80} src={"/TypeScript.png"} alt="TypeScript icon" unoptimized={true} />
+                        <Image className="mx-auto my-4" width={80} height={80} src={"/TypeScript.png"} alt="TypeScript icon" />
                         <p className="my-4">TypeScript</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] rounded-md hover:scale-110 duration-500 bg-[#ccd6f6]">
-                        <Image className="mx-auto my-4" width={80} height={80} src={"/AWS.png"} alt="AWS icon" unoptimized={true} />
+                        <Image className="mx-auto my-4" width={80} height={80} src={"/AWS.png"} alt="AWS icon" />
                         <p className="my-4">AWS</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] rounded-md hover:scale-110 duration-500 bg-[#ccd6f6]">
-                        <Image className="mx-auto my-4" width={80} height={80} src={"/NextJS.png"} alt="NextJS icon" unoptimized={true} />
+                        <Image className="mx-auto my-4" width={80} height={80} src={"/NextJS.png"} alt="NextJS icon" />
                         <p className="my-4">NextJS</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] rounded-md hover:scale-110 duration-500 bg-[#ccd6f6]">
-                        <Image className="mx-auto my-4" width={80} height={80} src={"/SQL.png"} alt="SQL icon" unoptimized={true} />
+                        <Image className="mx-auto my-4" width={80} height={80} src={"/SQL.png"} alt="SQL icon" />
                         <p className="my-4">SQL</p>
                     </div>
                     <div className="shadow-md shadow-[#040c16] rounded-md hover:scale-110 duration-500 bg-[#ccd6f6]">
-                        <Image className="mx-auto my-4" width={80} height={80} src={"/GitHub.png"} alt="GitHub icon" unoptimized={true} />
+                        <Image className="mx-auto my-4" width={80} height={80} src={"/GitHub.png"} alt="GitHub icon" />
                         <p className="my-4">GitHub</p>
                     </div>
                 </div>
